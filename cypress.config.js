438778a1ecc8e37d63b7d94281ee76e18edb6c0e
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2025-06-04 16:58:30 -0300

    adicionando mochawedome

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,29 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "szbr1p",
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results.json',
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Artemis',
+      overwrite: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      autoOpen: false,
+      quite: true,
+      html: false,
+      json: true
+    },
+  },
   e2e: {
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
+      // require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
     specPattern: [
